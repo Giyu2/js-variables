711eabbd0349a99e0fc95752f55c065ecb136a54
Giyu2 <derdene719@gmail.com>
2024-12-02 03:16:14 -0800

    jgdfioa

---
+++ index.js
@@ -0,0 +1,76 @@
+console.log("hello world from JS");
+const firstName="Dolgor"
+console.log(firstName);
+const maritakStatus=false;
+const age=19;
+console.log(age)
+
+
+
+const number=1
+const stringNumber ="1";
+console.log(number=== stringNumber)
+console.log(number== stringNumber)
+
+
+const a=20;
+const b=20;
+const c=30;
+console.log(a+b+c)
+
+
+
+const S=3.14*a^2;
+console.log(S)
+
+
+
+const y=6
+const x=10
+const m=y*2-y*1/x*2-x*1;
+console.log(m);
+
+
+
+console.log(15/3)
+
+
+
+const side1=10;
+const side2=10;
+const side3=10;
+const P=side1+side2+side3;
+const p=P/2;
+console.log(Math.sqrt(p*(p-side1)*(p-side2)*(p-side3)))
+
+
+const C=100
+const F=32
+console.log(C/F)
+
+
+const A1=96;
+const A2=108;
+const A3=89;
+const A=A1+A2+A3;
+console.log(A/3)
+const B1=88;
+const B2=91;
+const B3=110;
+const B=B1+B2+B3;
+console.log(B/3)
+
+
+
+
+const Q=8;
+const W=4;
+console.log(Q+W,Q-W,Q/W,Q*W,Q%W)
+
+const cm=8;
+const inch=2.54;
+console.log(8/2.54)
+
+
+
+const radius=prompt();
